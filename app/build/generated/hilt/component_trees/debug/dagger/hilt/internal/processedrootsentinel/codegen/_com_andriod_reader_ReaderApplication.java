db5fb5c0bf47497cb41d28803407fc3a3994cc39
dermotv5chat <dermotv5chat@proton.me>
2026-06-21 16:29:18 +0800

    chore: 移除冗余文件

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_andriod_reader_ReaderApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.andriod.reader.ReaderApplication"
-)
-public final class _com_andriod_reader_ReaderApplication {
-}
